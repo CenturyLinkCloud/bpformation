be45a8fbb435a22310baf85b9aeccff50b4ca6a5
Keith Resar <keith.resar@centurylinkcloud.com>
2015-05-13 10:49:09 -0500

    -

---
+++ src/setup.py
@@ -0,0 +1,33 @@
+
+#
+# python setup.py sdist
+# python setup.py bdist_dumb
+# python setup.py bdist_rpm
+#
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "bpformation",
+	version = "0.1",
+	packages = find_packages("."),
+
+	install_requires = ['prettytable','clint','argparse','requests'],
+
+	entry_points = {
+		'console_scripts': [
+			'bpformation  = bpformation.cli:main',
+		],
+	},
+
+
+	# metadata for upload to PyPI
+	author = "Keith Resar",
+	author_email = "Keith.Resar@CenturyLinkCloud.com",
+	description = "Blueprint Formation CLI",
+	keywords = "CenturyLink Cloud Blueprint CLI",
+	url = "https://github.com/CenturyLinkCloud/bp-formation",
+
+	# could also include long_description, download_url, classifiers, etc.
+)
+
