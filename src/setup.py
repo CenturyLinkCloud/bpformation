56018ad283996181fbcd6b41d453a62eab56cc4a
Keith Resar <keith.resar@centurylinkcloud.com>
2015-05-19 19:13:31 -0500

    -

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@ setup(
 	version = "0.1",
 	packages = find_packages("."),
 
-	install_requires = ['prettytable','clint','argparse','requests'],
+	install_requires = ['prettytable','clint','argparse','requests','lxml'],
 
 	entry_points = {
 		'console_scripts': [
