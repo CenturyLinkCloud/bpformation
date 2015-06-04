c8f73de5422207289804c890db0425468e81adbe
Keith Resar <keith.resar@centurylinkcloud.com>
2015-06-04 11:56:39 -0500

    -

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "bpformation",
-	version = "0.1",
+	version = "0.2",
 	packages = find_packages("."),
 
 	install_requires = ['prettytable','clint','argparse','requests','lxml','clc-sdk'],
